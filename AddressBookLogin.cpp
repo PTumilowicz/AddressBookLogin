73139535bc59f234dd94962c36ce1fc3e9942c91
PTumilowicz <80091707+PTumilowicz@users.noreply.github.com>
2023-04-22 11:12:07 +0200

    login and register user done

--- AddressBookLogin.cpp
+++ AddressBookLogin.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 #include <vector>
 
 using namespace std;
@@ -8,13 +9,13 @@ using namespace std;
 // Structs
 struct User
 {
-    int id;
+    int id = 0;
     string login, password;
 };
 
 struct Contact
 {
-    int id, userId;
+    int id = 0, userId = 0;
     string firstName, lastName, phone, address, email;
 };
 
@@ -90,7 +91,6 @@ void mainMenu()
     cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
     cout << "9. Change user's password" << endl;
     cout << "0. Log out" << endl;
-
 }
 
 void goodbyeMessage()
@@ -99,15 +99,126 @@ void goodbyeMessage()
 }
 
 // User functions
-
-int loginUser()
+void loadUsersFromFile(vector <User>& users)
 {
-    return 1;
+    string line, field;
+    User user;
+    ifstream file("users.txt", ios::app);
+
+    if (!file.is_open()) {
+        cout << "Failed to open file" << endl;
+        return;
+    }
+
+    while (getline(file, line)) {
+        istringstream iss(line);
+
+        getline(iss, field, '|');
+        user.id = stoi(field);
+
+        getline(iss, user.login, '|');
+        getline(iss, user.password, '|');
+
+        users.push_back(user);
+    }
+
+    file.close();
 }
 
-void registerUser()
+int loginUser(vector <User> &users)
 {
+    string login, password;
 
+    titleMessage();
+    cout << "Enter login: ";
+    login = readLine();
+
+    for (User &user : users)
+    {
+        if (user.login == login)
+        {
+            for (int i = 0; i < 3; ++i)
+            {
+                cout << "Enter password: ";
+                password = readLine();
+
+                if (user.password == password)
+                {
+                    cout << "User logged in." << endl;
+                    system("pause");
+                    return user.id;
+                }
+
+                cout << "Password incorrect. You have " << 2 - i << " trials left." << endl;
+
+                if (i == 2)
+                {
+                    cout << "You tried to login three times. You will be redirected to main menu." << endl;
+                    system("pause");
+                    return 0;
+                }
+            }
+        }
+    }
+
+    cout << "Login incorrect. Try again." << endl;
+    system("pause");
+
+    return 0;
+}
+
+bool checkUserLogin(vector <User>& users, string login)
+{
+    for (User& user : users)
+    {
+        if (user.login == login)
+        {
+            cout << "\nUsername already exist. Choose another username: " << endl;
+            system("pause");
+            return true;
+        }
+    }
+    return false;
+}
+
+void addUserToFile(User user)
+{
+    ofstream file("users.txt", ios::app);
+
+    if (!file.is_open()) {
+        cout << "Failed to open file" << endl;
+        return;
+    }
+
+    file << user.id << '|' << user.login << '|' << user.password << '|' << endl;
+
+    file.close();
+}
+
+void registerUser(vector <User>& users)
+{
+    bool loginExists = true;
+    User user;
+    user.id = users.empty() ? 1 : users.back().id + 1;
+
+    titleMessage();
+    
+    while (loginExists)
+    {
+        cout << "Enter your username: ";
+        user.login = readLine();
+
+        loginExists = checkUserLogin(users, user.login);
+    }
+    
+    cout << "Enter your password: ";
+    user.password = readLine();
+
+    users.push_back(user);
+    addUserToFile(user);
+
+    cout << "\nUser registered!\n" << endl;
+    system("pause");
 }
 
 int main()
@@ -118,6 +229,8 @@ int main()
     vector <User> users;
     vector <Contact> contacts;
 
+    loadUsersFromFile(users);
+
     while(menuFlag)
     {
         titleMessage();
@@ -127,10 +240,10 @@ int main()
         switch (choice)
         {
         case '1':
-            loggedUserId = loginUser();
+            loggedUserId = loginUser(users);
             break;
         case '2':
-            registerUser();
+            registerUser(users);
             break;
         case '3':
             goodbyeMessage();
@@ -165,7 +278,7 @@ int main()
             default:
                 cout << "There is no such option. Try again." << endl;
                 system("pause");
-            }
+            }        
         }
     }
     return 0;
